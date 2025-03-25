b93a0913f0b5665ffac7569b2b3bbd19f51b7198
Tim <timporgertim@gmail.com>
2025-03-25 16:25:06 +0100

    embedded DM Serif globally

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
-</script>
+  </script>
+  
+  <!-- here fonts are embedded globally -->
+  <svelte:head>
+	<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet" />
+  </svelte:head>
+  
 
 {@render children()}
