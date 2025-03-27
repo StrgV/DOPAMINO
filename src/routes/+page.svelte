b7c8cdbeed71f487816deba6aec3d9df0e75232c
Tim <timporgertim@gmail.com>
2025-03-27 16:23:45 +0100

    Basic styling for the login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>DOPAMINO</h1>
+<h2 class="italic">~wir sind dein Casino</h2>
 
 <form method="POST">
     <input name="username" placeholder="Username" required />
