b5e1ab36266903ab7d0c47328ae5a68561751004
Tim <timporgertim@gmail.com>
2025-04-03 13:39:27 +0200

    Hamburger component

---
+++ src/lib/components/Hamburger.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let isOpen:boolean = false;
+
+  const toggleHamburger = () => {
+    isOpen = !isOpen;
+  };
+</script>
+
+<button 
+class="hamburger hamburger--squeeze {isOpen ? 'is-active' : ''}" 
+type="button"
+on:click={toggleHamburger}
+aria-label="Menu" 
+aria-controls="navigation">
+  <span class="hamburger-box">
+    <span class="hamburger-inner"></span>
+  </span>
+</button>
--- src/routes/casino/+page.svelte
+++ src/routes/casino/+page.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
     import logo from "$lib/assets/DOPAMINO_Text_Logo.svg";
-
-    let isOpen:boolean = false;
-
-    const toggleHamburger = () => {
-        isOpen = !isOpen;
-    };
+    import Hamburger from "$lib/components/Hamburger.svelte";
 </script>
 
 <img src="{logo}" alt="Logo of DOPAMINO" />
 <p>Enjoy your stay!</p>
-<button 
-class="hamburger hamburger--squeeze {isOpen ? 'is-active' : ''}" 
-type="button"
-on:click={toggleHamburger}
-aria-label="Menu" 
-aria-controls="navigation">
-  <span class="hamburger-box">
-    <span class="hamburger-inner"></span>
-  </span>
-</button>
+<Hamburger />
