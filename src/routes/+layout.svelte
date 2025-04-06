3927fdbd07caa81e2c688a1b6deef0ed2eb5f4ea
StrgV <timporgertim@gmail.com>
2025-04-06 13:09:37 +0200

    working hamburger

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
 	import '../app.css';
+  import {Hamburger} from 'svelte-hamburgers';
 	let { children } = $props();
+
+  let open: boolean = $state(false);
   </script>
   
   <svelte:head>
   <!-- embed fonts globally -->
   	<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet" />
-    <!-- for hamburger menu usage -->
-     <link href="src/hamburgers.css" rel="stylesheet">
   </svelte:head>
   
+  <nav>
+    <Hamburger bind:open type="squeeze" title="Menü" --color="var(--secondary-color)" ariaControls="nav"/>
+
+    {#if open}
+      <ul>
+        <li>Test1</li>
+        <li>Test2</li>
+        <li>Test3</li>
+      </ul>     
+    {/if}
+
+  </nav>
 
 {@render children()}
