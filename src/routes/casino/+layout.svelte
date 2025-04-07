f1635937e9d65043db932c2fd0ae084625b8a4ce
Tim <timporgertim@gmail.com>
2025-04-07 18:12:20 +0200

    Hamburger Menu moved to casino layout file and blurring on open menu for better visibility

---
+++ src/routes/casino/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {Hamburger} from 'svelte-hamburgers';
+  let open: boolean = $state(false);
+  let {children} = $props();
+</script>
+
+  <nav>
+    <Hamburger bind:open type="squeeze" title="Menü" --color="var(--secondary-color)" ariaControls="nav"/>
+
+    {#if open}
+      <ul>
+        <li>Eins</li>
+        <li>Zwei</li>
+        <li><a href="/register">Registrierung</a></li>
+      </ul>     
+    {/if}
+
+  </nav>
+
+  <main class:blur={open}>
+    {@render children()}
+  </main>
