fdcb7766427a097305aaf84c8b93b3363d8a1b88
Tim <timporgertim@gmail.com>
2025-04-10 19:11:48 +0200

    Fix: hamburger closes when changing sites, also when clicking anywhere and when pressing esc

--- src/routes/casino/+layout.svelte
+++ src/routes/casino/+layout.svelte
@@ -1,6 +1,38 @@
 <script lang="ts">
   import {Hamburger} from 'svelte-hamburgers';
+  import {onMount, onDestroy} from 'svelte';
   let open: boolean = $state(false);
+
+  const closeMenu = () => {
+    open = false;
+  };
+
+  // Close hamburger on click, or esc-key
+  const handleMouseClick = (event: MouseEvent) => {
+    closeMenu();
+  };
+
+  const handleEsc = (event: KeyboardEvent) => {
+    if (event.key === 'Escape'){
+      closeMenu();
+    }
+  };
+
+  // Add eventlisteners
+  onMount(() => {
+    if (typeof document !== 'undefined') {
+      document.addEventListener('click', handleMouseClick);
+      document.addEventListener('keydown', handleEsc);
+    }
+  });
+
+  onDestroy(() => {
+    if (typeof document !== 'undefined') {
+      document.removeEventListener('click', handleMouseClick);
+      document.removeEventListener('keydown', handleEsc);
+    }
+  });
+
   let {children} = $props();
 </script>
 
@@ -10,6 +42,7 @@
     {#if open}
       <ul>
         <li><a href="/casino/black-jack">Black Jack</a></li>
+        <li><a href="/casino">Home</a></li>
         <li><a href="/register">Registrierung</a></li>
         <li><a href="https://www.bundesweit-gegen-gluecksspielsucht.de/">Geld Aufladen</a></li>
       </ul>     
