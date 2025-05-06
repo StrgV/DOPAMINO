6a060f4026c700a351169d375c8c6b50e0dd2000
StrgV <timporgertim@gmail.com>
2025-05-06 21:22:14 +0200

    Card Components

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+    export let value: string;
+    export let suit: string;
+
+    function suitToChar(suit: string): string {
+        switch (suit) {
+            case '♥️': return 'H';
+            case '♦️': return 'D';
+            case '♣️': return 'C';
+            case '♠️': return 'S';
+            default: return '';
+        }
+    }
+
+    const imagePath = `/Cards/Card_${value}${suitToChar(suit)}.svg`;
+
+</script>
+
+<div class="card">
+    <img src={imagePath} alt={`Playing card: ${value}${suit}`} />
+</div>
+
+<style>
+    .card img {
+        display: flex; 
+        width: auto;
+        height: auto;
+        filter: drop-shadow(0px 10px 10px black);
+    }
+</style>
---
+++ src/routes/casino/card-test/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { type Card, createDeck} from '../black-jack/blackjack';
+    import CardComponent from '$lib/components/Card.svelte';
+       
+    let hand: Card[] = createDeck(1);
+    let sampleHand: Card[] = [hand[1], hand[32], hand[22]];
+
+export function suitToChar(suit: string): string {
+    switch (suit) {
+        case '♥️':
+            return 'H';
+        case '♦️':
+            return 'D';
+        case '♣️':
+            return 'C';
+        case '♠️':
+            return 'S';
+        default:
+            return '';
+    }
+}
+
+</script>
+
+<h1>Card_test</h1>
+
+
+
+<div class="hand">
+    
+    {#each sampleHand as card}
+        <CardComponent value={card.value} suit={card.suit} />
+    {/each}
+    
+</div>
+
+
+
+<style>
+    .hand{
+        display: flex;
+        height: 50vh;
+        width: 50vh;
+        align-items: center;
+        justify-content: center;
+    }
+
+</style>
