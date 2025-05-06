925cbd757b0c323aa829e5b0c242cb1e3381c596
StrgV <timporgertim@gmail.com>
2025-05-06 18:28:58 +0200

    Rename J to B

--- src/routes/casino/black-jack/blackjack.ts
+++ src/routes/casino/black-jack/blackjack.ts
@@ -1,6 +1,6 @@
 export type Card = {
     suit: '♥️' | '♦️' | '♣️' | '♠️';
-    value: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+    value: '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'B' | 'Q' | 'K' | 'A';
 };
 
 export function createDeck(deckCount = 2): Card[] {
