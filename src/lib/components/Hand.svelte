4a3537ecc7da50e3aef362400f784e855be6a275
StrgV <timporgertim@gmail.com>
2025-05-13 11:13:04 +0200

    Add Hand component which takes cards

---
+++ src/lib/components/Hand.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import CardComponent from '$lib/components/Card.svelte';
+
+    let { hand } = $props();
+    
+    const overlap = 50;
+
+</script>
+
+
+<div class="hand">
+    
+    {#each hand as card, i}
+        {@const marginLeft = i > 0 ? -overlap : 0}
+        {@const dynamicStyle = `margin-left: ${marginLeft}px`}
+
+        <CardComponent 
+            value={card.value} 
+            suit={card.suit} 
+            style={dynamicStyle}/>
+    {/each}
+    
+</div>
+
+<style>
+    .hand{
+        display: flex;
+        height: 50vh;
+        width: 50vh;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
